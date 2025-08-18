50cfa2cf47b044fa40ee2c8f7db1ee44e2768187
Prof. Charles Pereira <charlespereira.fln@gmail.com>
2025-08-18 15:01:39 -0300

    feature: template-ver_produtos.html

--- produtos/urls.py
+++ produtos/urls.py
@@ -4,4 +4,5 @@ from . import views  # Importa as views da nossa app 'produtos'
 
 urlpatterns = [
     path('ver_produto/', views.ver_produto, name='ver_produto'),
+    path('inserir_produto/', views.inserir_produto, name='inserir_produto'),
 ]
--- produtos/views.py
+++ produtos/views.py
@@ -1,10 +1,15 @@
+from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 
 def ver_produto(request):
+    return render(request, 'ver_produto.html', {'nome': 'Produto Exemplo'})
+
+
+def inserir_produto(request):
     return HttpResponse(
-        "<h1>Detalhes do Produto</h1>"
-        "<p>Este é o produto que você está visualizando.</p>"
+        "<h1>Inserir Produto</h1>"
+        "<p>Esta é a página para inserir um novo produto.</p>"
     )
