0adb023905620703ef1fe326424ae5a66231ed6e
Prof. Charles Pereira <charlespereira.fln@gmail.com>
2025-08-18 16:52:15 -0300

    feature: formularios

--- produtos/views.py
+++ produtos/views.py
@@ -5,7 +5,19 @@ from django.http import HttpResponse
 
 
 def ver_produto(request):
-    return render(request, 'ver_produto.html', {'nome': 'Produto Exemplo'})
+    if request.method == 'GET':
+        return render(request, 'ver_produto.html', {'nome': 'Produto Exemplo'})
+    elif request.method == 'POST':
+        nome = request.POST.get('nome')
+        quantidade = request.POST.get('quantidade')
+
+        return HttpResponse(
+            (
+                f"<h1>Cadastro de Produtos</h1>"
+                f"<p>O produto '{nome}' (quantidade: {quantidade}) "
+                "foi cadastrado com sucesso!</p>"
+            )
+        )
 
 
 def inserir_produto(request):
